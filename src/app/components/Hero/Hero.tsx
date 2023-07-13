60002efcfe3303b81ebe76cd51cdfc778094410b
Luis <hola@luiscaballe.com>
2023-07-13 14:10:16 +0200

    Add Hero css module

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,19 +1,24 @@
 import React from "react";
+import styles from "./Hero.module.css";
 import pageStyles from "../../page.module.css";
 
 const Hero = () => {
   return (
-    <section className={pageStyles.hero_section}>
-      <p className={pageStyles.content_text}>Hi there! My name is</p>
-      <h2>Luis Caballé.</h2>
+    <section className={styles.hero_section}>
+      <p>Hi there! My name is</p>
       <h2>
-        {`I'm a Graphic Designer`}
+        Luis Caballé<span className={pageStyles.secondary_text}>.</span>
         <br />
-        and a Full Stack Developer
+        I&apos;m a{" "}
+        <span className={pageStyles.secondary_text}>Graphic Designer</span>
+        <br />
+        and a{" "}
+        <span className={pageStyles.secondary_text}>Full Stack Developer</span>.
       </h2>
-      <p className={pageStyles.content_text}>
-        With over ten years of experience in various design disciplines, I’m
-        your one-stop shop for your design needs.
+      <p>
+        With over ten years of experience in various design disciplines,
+        I&apos;ve decided to introduce myself to the passionate world of web
+        development. Always eager to learn and with a smile on my face.
       </p>
     </section>
   );
