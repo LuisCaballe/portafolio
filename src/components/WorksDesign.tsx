2694c71d25f9327a8c0dd60cb7d83dafd39c9ded
Luis <hola@luiscaballe.com>
2023-08-01 15:47:22 +0200

    Add design projects

--- src/components/WorksDesign.tsx
+++ src/components/WorksDesign.tsx
@@ -11,7 +11,7 @@ const WorksDesign = (): React.ReactElement => {
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-weedom.jpg"
                 fill
                 alt="Weedom"
                 sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
@@ -19,140 +19,197 @@ const WorksDesign = (): React.ReactElement => {
               <div className="card__info">
                 <h3>Weedom</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/dd-tea">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-dd-tea.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Delideluxe tea menu"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Delideluxe</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>tea menu design</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/mqp">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-mqp.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Més que paraules"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Més Que Paraules</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/sentits-bcn">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-sentits-bcn.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Sentits Bcn"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Sentits Bcn</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/logos">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-logos.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Sentits Bcn"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Logos</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>Logos design</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/rodrigo-50">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-rodrigo-50.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Rodrigo 50"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Rodrigo 50</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/airam">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-airam.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Airam"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Airam</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
         </article>
 
         <article className="card">
-          <Link href="/design-projects/weedom">
+          <Link href="/design-projects/dd-menu">
             <div className="card__image-container card__image-container--design">
               <Image
                 className="card__image"
-                src="/images/design-projects/home-page/weedom.jpg"
+                src="/images/design-projects/thumbs/thumb-dd-menu.jpg"
                 fill
-                alt="Weedom"
-                sizes="(max-width: 850px) 75vw, (max-width: 1200px) 30vw"
+                alt="Delideluxe'menu"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
               />
               <div className="card__info">
-                <h3>Weedom</h3>
+                <h3>Delideluxe</h3>
                 <span className="card__info-separator"></span>
-                <p>Brand identity</p>
+                <p>menu design</p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="card">
+          <Link href="/design-projects/brotes-verdes">
+            <div className="card__image-container card__image-container--design">
+              <Image
+                className="card__image"
+                src="/images/design-projects/thumbs/thumb-brotes-verdes.jpg"
+                fill
+                alt="Brotes Verdes"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
+              />
+              <div className="card__info">
+                <h3>Brotes Verdes</h3>
+                <span className="card__info-separator"></span>
+                <p>corporate identity</p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="card">
+          <Link href="/design-projects/salva">
+            <div className="card__image-container card__image-container--design">
+              <Image
+                className="card__image"
+                src="/images/design-projects/thumbs/thumb-salva.jpg"
+                fill
+                alt="Salva a caga"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
+              />
+              <div className="card__info">
+                <h3>Salva a caga</h3>
+                <span className="card__info-separator"></span>
+                <p>corporate identity</p>
+              </div>
+            </div>
+          </Link>
+        </article>
+
+        <article className="card">
+          <Link href="/design-projects/gmm">
+            <div className="card__image-container card__image-container--design">
+              <Image
+                className="card__image"
+                src="/images/design-projects/thumbs/thumb-gmm.jpg"
+                fill
+                alt="Grupo Much More"
+                sizes="(max-width: 510px) 65vw, (max-width: 800px) 40vw, 26vw "
+              />
+              <div className="card__info">
+                <h3>Grupo Much More</h3>
+                <span className="card__info-separator"></span>
+                <p>corporate identity</p>
               </div>
             </div>
           </Link>
